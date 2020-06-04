4b13de7919abf01495edf52c503dc412cf34db2f
gerson93 <60151381+gerson93@users.noreply.github.com>
2020-06-03 21:57:47 -0400

    Update screenshotmatic.py

--- screenshotmatic.py
+++ screenshotmatic.py
@@ -1,11 +1,6 @@
 import pyautogui
 from datetime import datetime, time, timedelta
 
-time_next_screenshot = time(21, 36, 0)
-now = datetime.now()
-now = time(now.hour, now.minute, 0)
-number_of_cap = 0
-
 def get_now_time ():
     now = datetime.now()
     return time(now.hour, now.minute, 0)
@@ -16,14 +11,19 @@ def add_delta (now, step):
     result = generated_date + step
     return time(result.hour, result.minute, 0)
 
-while (True):
-    now = get_now_time()
-    if (now == time_next_screenshot):
-        print('sacar captura')
-        screenshot = pyautogui.screenshot()
-        screenshot.save("D:\ cap" + str(number_of_cap) + ".png")
-        time_next_screenshot = add_delta (now, 5)
-        number_of_cap += 1
-        print(time_next_screenshot)
-    if (now > time(21, 38, 0)):
-        break
+def screenshotmatic (endtime, step):
+    time_next_screenshot = add_delta(get_now_time(), 1)
+    number_of_cap = 0
+    while (True):
+        now = get_now_time()
+        if (now == time_next_screenshot):
+            print('sacar captura')
+            screenshot = pyautogui.screenshot()
+            screenshot.save("D:\ cap" + str(number_of_cap) + ".png")
+            time_next_screenshot = add_delta (now, step)
+            number_of_cap += 1
+            print(time_next_screenshot)
+        if (now > endtime):
+            break
+
+screenshotmatic(time(22, 0, 0), 5) #Until 22:00, every 5 minutes
